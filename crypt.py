bb3a93689133b3ff2c2247de022cc63839d8f96d
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-04 05:07:48 -0700

    check hashlib

--- crypt.py
+++ crypt.py
@@ -1,4 +1,5 @@
 # This project will be about the python encryption and decryption of files and text
 
 # This project will be consisting of various methods to encrypt and decrypt
-
+import hashlib
+print(hashlib.algorithms_available)
