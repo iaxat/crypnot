822635fdcaa9b975176e94a093b66f0d3645bb44
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-04 17:06:04 -0700

    up

--- crypt.py
+++ crypt.py
@@ -1,6 +1,1 @@
-# This project will be about the python encryption and decryption of files and text
-
-# This project will be consisting of various methods to encrypt and decrypt
-import hashlib
-print(hashlib.algorithms_available)
-
+# Encrypt and Decrypt the Text Files
